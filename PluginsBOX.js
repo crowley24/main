42fc7ac79e18d11cab3831d2efd9f1f0133f24f5
crowley24 <unclezhenya26@gmail.com>
2026-03-08 23:22:22 +0200

    Update PluginsBOX.js

--- PluginsBOX.js
+++ PluginsBOX.js
@@ -1173,6 +1173,75 @@ Lampa.SettingsApi.addParam({
         });
     }
 });
+        Lampa.SettingsApi.addParam({
+    component: 'add_plugin',
+    param: {
+        name: 'Buttons Manager', // Назва в списку вибору
+        type: 'select',
+        values: {
+            1: 'Встановити',
+            2: 'Видалити',
+        },
+    },
+    field: {
+        name: 'Buttons Manager', // Назва в налаштуваннях
+        description: 'Кастомізація іконок медіакнопок' // Ваш новий опис
+    },
+    onChange: function (value, item) { 
+        // Посилання на Raw-файл v1
+        var pluginUrl = 'https://crowley24.github.io/main/ButtonsManager.js ';
+        var pluginName = 'Buttons Manager';
+        var index = $(item).data('nthChildIndex'); 
+
+        if (value == '1') {
+            itemON(pluginUrl, pluginName, '@crowley', pluginName, index); 
+        }
+        
+        if (value == '2') {
+            deletePlugin(pluginUrl, index);
+        }
+    },
+    onRender: function (item) { 
+        $('.settings-param__name', item).css('color', '#f3d900');
+        hideInstall();
+        
+        var pluginUrl = 'https://crowley24.github.io/main/ButtonsManager.js';
+        var pluginName = 'Buttuns Manager';
+        var myResult = checkPlugin(pluginUrl);
+        var pluginsArray = Lampa.Storage.get('plugins') || [];
+        
+        setTimeout(function () {
+            // Додаємо індикатор статусу (кольоровий кружечок)
+            $('div[data-name="' + pluginName + '"]').append('<div class="settings-param__status one"></div>');
+            var pluginStatus = null;
+            
+            for (var i = 0; i < pluginsArray.length; i++) {
+                if (pluginsArray[i].url === pluginUrl) {
+                    pluginStatus = pluginsArray[i].status;
+                    break;
+                }
+            }
+            
+            var statusElem = $('div[data-name="' + pluginName + '"]').find('.settings-param__status');
+            
+            if (myResult && pluginStatus !== 0) {
+                // Зелений - встановлено та активний
+                statusElem.removeClass('active error').css('background', 'linear-gradient(45deg, #11e400, #36a700)');
+            } else if (pluginStatus === 0) {
+                // Помаранчевий - вимкнено
+                statusElem.removeClass('active error').css('background', 'linear-gradient(45deg, #ff8c00, #d96e00)');
+            } else {
+                // Червоний - не встановлено
+                statusElem.removeClass('active error').css('background', 'linear-gradient(45deg, #ff0000, #c40000)');
+            }
+        }, 100);
+
+        item.on("hover:enter", function (event) {
+            var localNthChildIndex = focus_back(event);
+            $(this).data('nthChildIndex', localNthChildIndex);
+        });
+    }
+});
         
         Lampa.SettingsApi.addParam({
             component: 'add_plugin',
